5cb30fd72ca7eb1539748c67475fdd61f68bda7f
EdharKruk <s26827@pjwstk.edu.pl>
2024-04-25 22:27:41 +0300

    Data base

---
+++ Task7/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Task7.Models;
+
+public class Order
+{
+    [Required]
+    public int IdOrder { get; set; }
+    [Required]
+    public int IdProduct { get; set; }
+    [Required]
+    public int Amount { get; set; }
+    [Required]
+    public DateTime CreatedAt { get; set; }
+    [Required]
+    public DateTime? FulfilledAt { get; set; }
+}
---
+++ Task7/Models/Warehouse.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Task7.Models;
+
+public class Warehouse
+{
+    [Required]
+    public int IdWarehouse { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public string Address { get; set; }
+}
