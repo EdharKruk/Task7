2d88f0efc94995715642628b0f9a29d4a76981c5
EdharKruk <s26827@pjwstk.edu.pl>
2024-04-25 22:47:44 +0300

    Controller

---
+++ Task7/Controller/WarhouseController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task7.Controller;
+
+public class WarhouseController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WarehouseController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public WarehouseController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+    }
+}
